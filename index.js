8ca87d3a0d71f6a7a13c9b7e17e4aea17afafc01
Chris Butcher <c.butcher@hotmail.com>
2019-01-05 13:31:17 -0800

    Added express, pug and stylesheets.

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/web', express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'The Maze Generator'
+    });
+});
+
+app.listen(port, () => console.log(`Maze generator listening on port ${port}!`));
