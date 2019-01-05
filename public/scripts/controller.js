1bc1305cd6971b8f0a74dbe316b2e579fb52adbf
Chris Butcher <c.butcher@hotmail.com>
2019-01-05 15:54:05 -0800

    Moved script that has site specific js to its own file.

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -3,16 +3,13 @@ function Controller(width, height, cellSize) {
     this.height   = height;
     this.cellSize = cellSize;
     this.maze     = new Maze(width, height, cellSize);
-    this.canvas   = null;
 
     this.inputs = {
         width: null,
         height: null,
         cellSize: null,
-    };
-
-    this.buttons = {
         restart: null,
+        save: null,
     };
 
     this.initialize = function(inputs) {
@@ -78,37 +75,5 @@ function Controller(width, height, cellSize) {
         }
 
         this.maze.render();
-    }
-}
-
-let controller = new Controller(1280, 720, 40);
-
-function setup() {
-
-    $('#controller').dialog({
-        resizable: false,
-        minimizable: true,
-        minimizeLocation: 'right',
-        width: 220,
-    });
-
-    controller.initialize({
-        width: document.getElementById('maze-width'),
-        height: document.getElementById('maze-height'),
-        cellSize: document.getElementById('maze-cell-size'),
-        restart: document.getElementById('maze-restart'),
-        save: document.getElementById('maze-save'),
-    });
-}
-
-let timer = null;
-function draw() {
-    background(255);
-    color(0);
-
-    controller.draw();
-}
-
-function updateWindow() {
-    resizeCanvas(maze.width, maze.height);
+    };
 }
---
+++ public/scripts/main.js
@@ -0,0 +1,30 @@
+let controller = new Controller(1280, 720, 40);
+
+function setup() {
+
+    jQuery('#controller').dialog({
+        resizable: false,
+        minimizable: true,
+        minimizeLocation: 'right',
+        width: 220,
+    });
+
+    controller.initialize({
+        width: document.getElementById('maze-width'),
+        height: document.getElementById('maze-height'),
+        cellSize: document.getElementById('maze-cell-size'),
+        restart: document.getElementById('maze-restart'),
+        save: document.getElementById('maze-save'),
+    });
+}
+
+function draw() {
+    background(255);
+    color(0);
+
+    controller.draw();
+}
+
+function updateWindow() {
+    resizeCanvas(maze.width, maze.height);
+}
