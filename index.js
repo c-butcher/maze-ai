6950e5173bd5f430f5a1011977751ea42fdebb13
Chris Butcher <c.butcher@hotmail.com>
2019-01-05 16:15:07 -0800

    Added helmet for security.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 const port = 3000;
 
+app.use(helmet());
 
 app.use('/web', express.static('public'));
 app.use('/web/p5', express.static('node_modules/p5/lib'));
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+app.post('/save', (req, res) => {
+
+});
 
 app.get('/', (req, res) => {
     res.render('index', {
